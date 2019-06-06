e786598427cc41106f89633b6d95f79e318db41d
vinibatista <vinibatista420@gmail.com>
2019-06-06 00:23:55 -0300

    aqui vai a edilçao do atendimento html e a classe controler do atendimento

---
+++ src/main/java/br/com/sas/controller/AtendimentoController.java
@@ -0,0 +1,25 @@
+package br.com.sas.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.sas.service.AlergiaService;
+import br.com.sas.service.AtendimentoService;
+import br.com.sas.service.RemedioService;
+
+public class AtendimentoController {
+
+	@Autowired
+	private AtendimentoService atendimentoService;
+	
+	@Autowired
+	private RemedioService remedioService;
+	
+	@Autowired
+	private AlergiaService alergiaService;
+	
+	@GetMap
+	
+	
+	
+	
+}
