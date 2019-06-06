a207bc9f22568815436dcb33a956a6e6df89b030
User <User@DESKTOP-2B99RT5>
2019-06-05 23:08:30 -0300

    [update] script de validação de campos alfanuméricos adicionado; cadastro-medico completamente validado.

--- src/main/resources/static/js/javaScript.js
+++ src/main/resources/static/js/javaScript.js
@@ -1,28 +1,36 @@
 $(document).ready(function() {
 	var $campo = $("#inputCEP");
-	$campo.mask('00000-000', {reverse : true});
+	$campo.mask('00000-000', { reverse : true });
 });
 
 $(document).ready(function() {
 	var $campo = $("#inputCPF");
-	$campo.mask('000.000.000-00', {reverse : true});
+	$campo.mask('000.000.000-00', { reverse : true });
 });
 
 function apenasNumerosOuX(e) {
 	var unicode = e.charCode ? e.charCode : e.keyCode
-	if (unicode != 8) { 
-		if (unicode < 48 || unicode > 57){
-			if (unicode != 88 && unicode != 120){
+	if (unicode != 8) {
+		if (unicode < 48 || unicode > 57) {
+			if (unicode != 88 && unicode != 120) {
 				return false
 			}
-		} 
+		}
 	}
 }
 
 function apenasNumeros(e) {
 	var unicode = e.charCode ? e.charCode : e.keyCode
-	if (unicode != 8) { //se a key não for o backspace
-		if (unicode < 48 || unicode > 57) //se não for um número
-			return false //desabilita o keypress -> cancela a inserção do caracter digitado
+	if (unicode != 8) { // se a key não for o backspace
+		if (unicode < 48 || unicode > 57) // se não for um número
+			return false // desabilita o keypress -> cancela a inserção do
+							// caracter digitado
 	}
 }
+
+$(".validaLetras").on("input", function(){
+	  var regexp = /[^a-zA-Z\u00C0-\u00FF ]/g;
+	  if(this.value.match(regexp)){
+	    $(this).val(this.value.replace(regexp,''));
+	  }
+});
